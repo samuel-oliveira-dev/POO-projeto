55ec6308f01f62db85b05b5aaa00ed318a605c3e
Samuel Oliveira <oliveirasamuel1971@gmail.com>
2021-10-30 19:46:51 -0300

    classes Produto, Pessoa e Livro criadas

---
+++ Pessoa.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema;
+
+/**
+ *
+ * @author samuk
+ */
+public class Pessoa {
+    
+    public String nome;
+    public String data;
+    public String logradouro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public long getCep() {
+        return cep;
+    }
+
+    public void setCep(long cep) {
+        this.cep = cep;
+    }
+    public long cpf;
+    public long cep;
+    
+}
