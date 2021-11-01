92ff336c416dba3594247bc8e74607bf9905c1e5
Samuel Oliveira <oliveirasamuel1971@gmail.com>
2021-11-01 18:20:54 -0300

    interface de cadastro de produtos criada

---
+++ telas/Livro.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package telas;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author samuk
+ */
+public class Livro extends Produto {
+    
+    private String titulo;
+    private String autor;
+    private String paginas;
+    private String isbn;
+    private String editora;
+    private String ano;
+    private String edicao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(String paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+    
+    
+    public String salvar(){
+        
+        FileWriter fw;
+        try {
+            fw = new FileWriter("livros.txt", true);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(getTitulo());
+            pw.println(getAutor());
+            pw.println(getEditora());
+            pw.println(getIsbn());
+            pw.println(getEdicao());
+            pw.println(getPaginas());
+            pw.println(getAno());
+            pw.flush();
+            pw.close();
+            fw.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Livro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return "Cadastrado com sucesso!";
+    }
+    
+    
+    
+    
+}
