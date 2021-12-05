1854f88856dafc040982a6e9300e49a7c5e4ddcf
ValterSantos23 <valterfabriciosantos2018@gmail.com>
2021-12-05 08:51:50 +0000

    tela de vendas

--- src/telas/TelaVenda.java
+++ src/telas/TelaVenda.java
@@ -16,6 +16,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JDesktopPane;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import regraNegocio.Cliente;
 import regraNegocio.Venda;
